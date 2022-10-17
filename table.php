3353bd0d31e366717758a686b1f28bafa88e6014
Marcella Grace Dulce <shiinamars@gmail.com>
2022-10-17 16:19:27 +0800

    Initial commit

---
+++ table.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Table</title>
+ <style>
+  table, th, td {
+  border: 2px solid black;
+  border-collapse: collapse;
+}
+ </style> 
+</head>
+<body>
+<?php
+    function build_table($array){
+    $html = '<table>';
+    $html .= '<tr>';
+    foreach($array[0] as $key=>$value){
+            $html .= '<th>' . htmlspecialchars($key) . '</th>';
+        }
+    $html .= '</tr>';
+
+    foreach( $array as $key=>$value){
+        $html .= '<tr>';
+        foreach($value as $key2=>$value2){
+            $html .= '<td>' . htmlspecialchars($value2) . '</td>';
+        }
+        $html .= '</tr>';
+    }
+
+    $html .= '</table>';
+    return $html;
+}
+$array = array(
+    array('Name'=>'Ramesh Raman ', 'Salary'=>' 5000'),
+    array('Salary'=>'Shabbir Hussein ', 'last'=>' 7000')
+);
+echo build_table($array);
+?>
+</body>
+</html>
